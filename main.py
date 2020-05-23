9b82b2163ff96a7bf2417986de6fb357e44ee75e
TaylorCairns <64628928+TaylorCairns@users.noreply.github.com>
2020-05-24 00:02:40 +1000

    update main.py

--- main.py
+++ main.py
@@ -1,196 +1,113 @@
-#main file
-from flask import Flask, render_template, request, url_for, session, redirect
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
-import socket
+from dbUtil import DatabaseUtils
 
-app = Flask(__name__)
+class Menu:
+    def main(self):
+        with DatabaseUtils() as db:
+            db.createBookingsTable()
+        self.runMenu()
 
+    def runMenu(self):
+        while(True):
+            print()
+            print("1. List Users")
+            print("2. List Cars")
+            print("3. Insert User")
+            print("4. Delete User")
+            print("5. Insert Car")
+            print("6. Delete Car")
+            print("7. Drop table")
+            print("8. Quit")
+            selection = input("Select an option: ")
+            print()
 
-# Didn't work without secret key, didn't matter what it is
-app.secret_key = 'your secret key'
+            if(selection == "1"):
+                self.listPeople()
+            elif(selection == "2"):
+                self.listCars()
+            elif(selection == "3"):
+                self.insertPerson()
+            elif(selection == "4"):
+                self.deletePerson()
+            elif(selection == "5"):
+                self.insertCar()
+            elif(selection == "6"):
+                self.deleteCar()
+            elif(selection == "7"):
+                #print("Have to change dbUtil if you want to drop a table")
+                self.dropTable()
+            elif(selection == "8"):
+                print("Goodbye!")
+                break
+            else:
+                print("Invalid input - please try again.")
 
-#create table if not exists Person (
-#                    PersonID int not null auto_increment,
-#                    Name text not null,
-#                    constraint PK_Person primary key (PersonID)
-#                )"""
-#
-#ursor.execute("""
-#                   CREATE TABLE IF NOT EXISTS `accounts` (
-#                   `id` int(11) NOT NULL AUTO_INCREMENT,
-#                   `username` varchar(50) NOT NULL,
-#                   `password` varchar(255) NOT NULL,
-#                   `email` varchar(100) NOT NULL,
-#                   PRIMARY KEY (`id`)
-#                   )""")
-
-# Enter your database connection details below
-app.config['MYSQL_HOST'] = '35.244.72.137'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234'
-app.config['MYSQL_DB'] = 'Peopl'
-
-#HOST = "35.244.72.137"
-#    USER = "root"
-#    PASSWORD = "1234"
-#    DATABASE = "Peopl"
-
-# Intialize MySQL
-mysql = MySQL(app)
-
-
-
-
-
-#@app.route('/')
-#def index():
-#    return 'Hello world'
-
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    # Output message if something goes wrong...
-    msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
-        # Fetch one record and return result
-        account = cursor.fetchone()
-        # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            # Redirect to home page
-            return redirect(url_for('home'))
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
+    def listPeople(self):
+        print("--- People ---")
+        print("{:<15} {}".format("Person ID", "Name"))
+        with DatabaseUtils() as db:
+            for person in db.getPeople():
+                print("{:<15} {}".format(person[0], person[1]))
         
-    return render_template('index.html', msg=msg)
+    def listCars(self):
+        print("--- Cars ---")
+        print("{:<15} {}".format("carId", "Make"))
+        with DatabaseUtils() as db:
+            for person in db.getCars():
+                #print("{:<15} {}".format(person[0]))
+                print(person)
+                
+    def dropTable(self):
+        print("----Drop Table----")
+        with DatabaseUtils() as db:
+            if(db.dropTable()):
+                print("dropped successfully.")
+            else:
+                print("didn't drop.")
 
-# http://localhost:5000/python/logout - this will be the logout page
-@app.route('/logout')
-def logout():
-    # Remove session data, this will log the user out
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('username', None)
-   # Redirect to login page
-   return redirect(url_for('login'))
-
-# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    # Output message if something goes wrong...
-    msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-        account = cursor.fetchone()
-        # If account exists show error and validation checks
-        if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form!'
-        else:
-            # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-            mysql.connection.commit()
-            msg = 'You have successfully registered!'
-    elif request.method == 'POST':
-        # Form is empty... (no POST data)
-        msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
-    return render_template('register.html', msg=msg)
-
-# http://localhost:5000/pythinlogin/home - this will be the home page, only accessible for loggedin users
-@app.route('/home')
-def home():
-    # Check if user is loggedin
-    if 'loggedin' in session:
-        # User is loggedin show them the home page
-        return render_template('home.html', username=session['username'])
-    # User is not loggedin redirect to login page
-    return redirect(url_for('login'))
-
-@app.route('/profile')
-def profile():
-    # Check if user is loggedin
-    if 'loggedin' in session:
-        # We need all the account info for the user so we can display it on the profile page
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
-        account = cursor.fetchone()
-        # Show the profile page with account info
-        return render_template('profile.html', account=account)
-    # User is not loggedin redirect to login page
-    return redirect(url_for('login'))
-
-@app.route('/cars')
-def cars():
-    # Check if user is loggedin
-    if 'loggedin' in session:
-        # We need all the account info for the user so we can display it on the profile page
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM cars')
-        cars = cursor.fetchall()
-        # Show the profile page with account info
-        return render_template('cars.html', cars=cars)
-    # User is not loggedin redirect to login page
-    return redirect(url_for('login'))
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
-
-
-##################Socket test
-localIP     = "127.0.0.1"
-localPort   = 20001
-bufferSize  = 1024
-
-msgFromServer       = "Hello UDP Client"
-otherMsgFromServer       = "no UDP Client"
-bytesToSend         = str.encode(msgFromServer)
-otherBytesToSend    = str.encode(otherMsgFromServer)
-
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-# Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
-print("UDP server up and listening")
-
-# Listen for incoming datagrams
-while(True):
-
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
+    def insertPerson(self):
+        print("--- Insert Person ---")
+        name = input("Enter the person's name: ")
+        with DatabaseUtils() as db:
+            if(db.insertPerson(name)):
+                print("{} inserted successfully.".format(name))
+            else:
+                print("{} failed to be inserted.".format(name))
     
-    print(clientMsg)
-    print(clientIP)
+    def deletePerson(self):
+        print("--- Delete Person ---")
+        id = input("Enter the person's ID: ")
+        with DatabaseUtils() as db:
+            if(db.deletePerson(id)):
+                print("{} deleted successfully.".format(id))
+            else:
+                print("{} failed to delete user.".format(id))
+    
+    def insertCar(self):
+        print("--- Insert Car ---")
+        carId = input("Enter the car's id: ")
+        make = input("Enter the car's make: ")
+        body = input("Enter the car's body type: ")
+        colour = input("Enter the car's colour: ")
+        seats = input("Enter the car's seat number: ")
+        location = input("Enter the car's location: ")
+        cost = input("Enter the car's cost per hour: ")
+        bookedBy = input("Enter the car's current owner, blank if no one: ")
+        
+        with DatabaseUtils() as db:
+            if(db.insertCar(carId, make, body, colour, seats, location, cost, bookedBy)):
+                print("{} inserted successfully.".format(make))
+            else:
+                print("{} failed to be inserted.".format(make))
+    
+    def deleteCar(self):
+        print("--- Delete Car ---")
+        id = input("Enter the car's ID: ")
+        with DatabaseUtils() as db:
+            if(db.deleteCar(id)):
+                print("{} deleted successfully.".format(id))
+            else:
+                print("{} failed to delete car.".format(id))
+    
 
-    # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
-
-
+if __name__ == "__main__":
+    Menu().main()
