4f1877a1af173a5e07c8219fe3a508a88b3477e5
rmit-s3602584-peter-moorhead <s3602584@student.rmit.edu.au>
2020-05-23 19:02:54 +1000

    changed api to diff file

--- api.py
+++ api.py
@@ -1,18 +1,18 @@
 #main file
 from flask import Flask, render_template, request, url_for, session, redirect
 from flask_mysqldb import MySQL
+from IoTAssignment2 import app
+from datetime import datetime, timedelta
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+#from cal_setup import get_calendar_service
 import MySQLdb.cursors
 import re
 import sys
-
-from datetime import datetime, timedelta
-from cal_setup import get_calendar_service
-
 import hashlib
-
-
-app = Flask(__name__)
-
+import pickle
+import os.path
 
 # Didn't work without secret key, didn't matter what it is
 app.secret_key = 'your secret key'
@@ -541,6 +541,32 @@ def userhistory():
             return render_template('cars.html')
     else:
         return redirect(url_for('login'))
- 
-if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+CREDENTIALS_FILE = 'credentials.json'
+
+def get_calendar_service():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CREDENTIALS_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+    return service
+
